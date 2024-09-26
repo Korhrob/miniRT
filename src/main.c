64d3b3ceecf3a87fb92a51cd0489b082ba57edef
Artur <vegis.artur@gmail.com>
2024-09-26 13:21:21 +0300

    main fix works

--- src/main.c
+++ src/main.c
@@ -10,124 +10,6 @@
 #include "image.h"
 #include "camera.h"
 #include "scene.h"
-
-int	main(void)
-{
-	t_image		image = init_image(16.0 / 9.0, 1280);
-	t_camera	camera = init_camera(vec3(0,5,-15), vec3(0, -2, -1), image, 60);
-	t_scene		scene;
-
-	scene.camera = &camera;
-	// Object list
-	t_list *list = 0;
-	t_list *temp = 0;
-	{
-		t_sphere *data;
-
-		data = new_sphere(vec3(-4.5, -1, 0), 1, vec3(1, 0, 0));
-		data->shape.id = 0;
-		temp = new_list(data, SPHERE);
-		list_add(&list, temp);
-
-		data = new_sphere(vec3(0, 17, -3), 20, vec3(0, 1, 0));
-		data->shape.id = 1;
-		temp = new_list(data, SPHERE);
-		list_add(&list, temp);
-	}
-	{
-		t_plane *data;
-
-		data = new_plane(vec3(0, -2, 0), vec3(10, 10, 10), vec3(0, 1, 0), vec3(1, 1, 1));
-		data->shape.id = 2;
-		temp = new_list(data, PLANE);
-		list_add(&list, temp);
-
-		data = new_plane(vec3(6, 4, 0), vec3(5, 10, 10), vec3(0, 1, 0), vec3(0, 0, 1));
-		data->shape.id = 4;
-		temp = new_list(data, PLANE);
-		list_add(&list, temp);
-	}
-	{
-		t_cylinder	*data;
-
-		data = new_cylinder(vec3(0, -1, 0), vec3(0.5, 1.0, 0.0), vec3(0, 1, 1), vec3(1, 0, 0));
-		data->shape.id = 6;
-		data->top->shape.id = 6;
-		data->bot->shape.id = 6;
-		temp = new_list(data, CYLINDER);
-		list_add(&list, temp);
-
-		temp = new_list(data->top, CYLINDER_CAP);
-		list_add(&list, temp);
-
-		temp = new_list(data->bot, CYLINDER_CAP);
-		list_add(&list, temp);
-
-		data = new_cylinder(vec3(2, -1, 0), vec3(0.5, 2.0, 0), vec3(0, 1, 0), vec3(1, 0, 0));
-		data->shape.id = 7;
-		data->top->shape.id = 7;
-		data->bot->shape.id = 7;
-		temp = new_list(data, CYLINDER);
-		list_add(&list, temp);
-
-		temp = new_list(data->top, CYLINDER_CAP);
-		list_add(&list, temp);
-
-		temp = new_list(data->bot, CYLINDER_CAP);
-		list_add(&list, temp);
-
-		data = new_cylinder(vec3(-2, -1, 0), vec3(1.0, 1.0, 0.0), vec3(1, 0, 1), vec3(1, 0, 0));
-		data->shape.id = 8;
-		data->top->shape.id = 8;
-		data->bot->shape.id = 8;
-		temp = new_list(data, CYLINDER);
-		list_add(&list, temp);
-
-		temp = new_list(data->top, CYLINDER_CAP);
-		list_add(&list, temp);
-
-		temp = new_list(data->bot, CYLINDER_CAP);
-		list_add(&list, temp);
-	}
-	scene.objects = list;
-	scene.light.pos = (t_point) { 0, 8, 0 };
-	scene.light.strength = 1;
-
-	scene.ambient.color = (t_color) { 1, 0, 0 };
-	scene.ambient.strength = 0.1;
-
-	render(image, &scene);
-
-	// list_clean
-	temp = list;
-	t_list *next = list->next;
-	while (temp != NULL)
-	{
-		next = temp->next;
-		//printf("type %d\n", temp->type);
-		if (temp->type == CYLINDER)
-			free_cylinder(temp->d.cylinder);
-		else if (temp->type == SPHERE)
-			free(temp->d.sphere);
-		else if (temp->type == PLANE)
-			free(temp->d.plane);
-		free(temp);
-		temp = next;
-	}
-}
-
-#include <fcntl.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include "minirt.h"
-#include "list.h"
-#include "sphere.h"
-#include "plane.h"
-#include "cylinder.h"
-#include "image.h"
-#include "camera.h"
-#include "scene.h"
 #include "../libft/libft.h"
 #include "parsing.h"
 
@@ -145,6 +27,25 @@ void displayProgressBar(int progress, int total) {
     printf("] %d%%\r", (int)(percentage * 100));  // Print the percentage complete and carriage return
 
     fflush(stdout);  // Flush the output buffer to ensure immediate display
+}
+
+void free_shape_list(t_list *list)
+{
+	t_list *next;
+	
+	while (list != NULL)
+	{
+		next = list->next;
+		//printf("type %d\n", temp->type);
+		if (list->type == CYLINDER)
+			free_cylinder(list->d.cylinder);
+		else if (list->type == SPHERE)
+			free(list->d.sphere);
+		else if (list->type == PLANE)
+			free(list->d.plane);
+		free(list);
+		list = next;
+	}
 }
 
 static void init_ambient(t_scene *scene, t_parse info)
@@ -188,7 +89,7 @@ static void init_cylinder(t_list **list, t_parse info, int *id)
 	t_list		*temp;
 	t_cylinder	*data;
 
-	data = new_cylinder(info.pos, info.v1, info.v2, info.orien, info.color);
+	data = new_cylinder(info.pos, vec3(info.v1, info.v2, 0), info.orien, info.color);
 	data->shape.id = *id;
 	data->top->shape.id = *id;
 	data->bot->shape.id = *id;
@@ -228,6 +129,7 @@ static void handle_info(t_scene *scene, t_camera *camera, t_image *image, int fd
 	int		id;
 
 	id = 0;
+	line = "";
 	while (line)
 	{
 		line = get_next_line(fd);
@@ -235,11 +137,20 @@ static void handle_info(t_scene *scene, t_camera *camera, t_image *image, int fd
 		{
 			set_info(line, &info);
 			if (info.id == ID_CAMERA)
+			{
+				printf("init camera\n");
 				*camera = init_camera(info.pos, info.orien, *image, info.v1);
+			}
 			if (info.id == ID_AMBIENT)
+			{
+				printf("init ambient\n");
 				init_ambient(scene, info);
+			}
 			if (info.id == ID_LIGHT)
+			{
+				printf("init light\n");
 				init_light(scene, info);
+			}
 			if (info.id == ID_CYLINDER || info.id == ID_PLANE || info.id == ID_SPHERE)
 				init_shape(&scene->objects, info, &id);
 		}
@@ -260,7 +171,7 @@ int	main(int argc, char **argv)
 		// error
 		return (1);
 	}
-	image = init_image(16.0 / 9.0, 400);
+	image = init_image(16.0 / 9.0, 1600);
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1)
 	{
@@ -269,7 +180,8 @@ int	main(int argc, char **argv)
 	}
 	scene.objects = 0;
 	handle_info(&scene, &camera, &image, fd);
-	render(camera, image, &scene);
+	scene.camera = &camera;
+	render(image, &scene);
 	close (fd);
 	return (0);
 }
